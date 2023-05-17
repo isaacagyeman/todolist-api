94cacd8420cc62fd2f89523cdfeda96e18ceb6d0
unknown <isaacagyeman437@gmail.com>
2023-05-17 22:17:30 +0100

    cleaned up code

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ try{
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// app.use(function(req,res){
-// res.status(404).send({url:req.originalUrl + 'not found'})
-// });
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
@@ -30,28 +27,3 @@ routes(app);
 app.listen(port);
 console.log('todo list RESTFUL ApI server started on: ' + port);
 
-/*
-
-const express = require('express')
-const app = express()
-
-// This is a demonstration of middleware in nodejs
-
-app.get('/api/agengo',(req,res)=>{
-res.send("Welcome to this shitty homepage")
-})
-
-app.post('/api/agengo',(req,res)=>{
-    const name = req.body
-    if(!name){
-        return res.status(400)
-                  .json({success:false, msg:'please enter a valid input'})
-    }else{
-        res.status(201).send({success:true, person:name})
-    }
-    
-})
-
-app.listen(5000);
-
-*/
